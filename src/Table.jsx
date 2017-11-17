3550f34668a65745e3cb2788ceb54f751b77afc0
Alisa Ono <alisao@mit.edu>
2017-11-17 13:29:28 -0500

    Better table style

--- src/Table.jsx
+++ src/Table.jsx
@@ -9,19 +9,34 @@ class Table extends React.Component {
   render() {
     const style = {
       container: {
-        float: "left",
+        border: "solid 1px rgba(0, 0, 0, 0.2)",
+        boxShadow: "-2px 2px 5px rgba(0, 0, 0, 0.2)",
+        display:"inline-block",
+        verticalAlign:"top",
+        padding:"20px 40px",
+        height: "45%",
+        overflow: "auto",
+      },
+      header: {
+        minWidth: "85px",
+        color: this.props.textColor,
+        fontSize: "0.9rem",
+        fontWeight: "700",
+        paddingBottom: "12px",
       },
       cell: {
-        minWidth: "100px",
-        color: "#000000"
+        minWidth: "75px",
+        color: this.props.textColor,
+        fontSize: "0.8rem",
+        fontWeight: "300",
       }
     }
 
     let rows = []
     rows.push(
       <tr>
-        <td style={style.cell}>ID:</td>
-        <td style={style.cell}>Title:</td>
+        <td style={style.header}>ID:</td>
+        <td style={style.header}>Title:</td>
       </tr>
     )
 
@@ -48,13 +63,15 @@ class Table extends React.Component {
 
 Table.defaultProps = {
   idKey: "id",
-  titleKey: "title"
+  titleKey: "title",
+  textColor: "#000000"
 }
 
 Table.propTypes = {
   paths: PropTypes.arrayOf(PropTypes.object).isRequired,
   idKey: PropTypes.string,
-  titleKey: PropTypes.string
+  titleKey: PropTypes.string,
+  textColor: PropTypes.string
 }
 
 export default Table
